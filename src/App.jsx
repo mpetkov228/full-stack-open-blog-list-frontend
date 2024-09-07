d8776f308bc8a3413ce6d0fa98a69b5d8ba86208
MihailPetkov96 <mpetkov228@gmail.com>
2024-09-07 21:16:50 +0300

    Add login input fields

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import blogService from './services/blogs';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     blogService.getAll().then(blogs => 
@@ -11,8 +13,32 @@ const App = () => {
     );
   }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          username <input 
+                     type="text" 
+                     name="username" 
+                     value={username}
+                     onChange={({ target }) => setUsername(target.value)}
+                   />
+        </div>
+        <div>
+          password <input 
+                     type="password" 
+                     name="password"
+                     value={password}
+                     onChange={({ target }) => setPassword(target.value)}
+                   />
+        </div>
+        <button type="submit">Log in</button>
+      </form>
       <h2>Blogs</h2>
       {blogs.map(blog => 
         <Blog key={blog.id} blog={blog} />
