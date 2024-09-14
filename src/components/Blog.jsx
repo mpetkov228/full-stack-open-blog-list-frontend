b83c850347db1c108d59340c2651bdbcc53678e9
MihailPetkov96 <mpetkov228@gmail.com>
2024-09-14 11:31:02 +0300

    Add expand view functionality for each blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const toggleView = () => setView(!view);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleView}>view</button>
+        {view 
+          ? <div>
+              <div>{blog.url}</div>
+              <div>likes {blog.likes} <button>like</button></div>
+              <div>{blog.author}</div>
+            </div>
+          : null}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
