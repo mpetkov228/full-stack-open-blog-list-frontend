5dafb0cfdd1612f7404645d34d690eab6cfcadea
MihailPetkov96 <mpetkov228@gmail.com>
2024-09-15 23:55:54 +0300

    Sort blogs by likes

--- src/App.jsx
+++ src/App.jsx
@@ -122,7 +122,9 @@ const App = () => {
         />
       </Togglable>
 
-      {blogs.map(blog => 
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog => 
         <Blog 
           key={blog.id} 
           blog={blog} 
